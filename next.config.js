5c54f1d22b6b17cae3142ad5d3f374ba88f2c025
Vivek Mali <vivekmali108@gmail.com>
2025-04-14 21:43:52 +0530

    chore: optimize next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -10,8 +10,15 @@ const nextConfig = {
       },
     ],
     domains: ['localhost'],
-    unoptimized: true,
+    unoptimized: false,
   },
+  compress: true,
+  poweredByHeader: false,
+  generateEtags: true,
+  onDemandEntries: {
+    maxInactiveAge: 60 * 60 * 1000,
+    pagesBufferLength: 5,
+  }
 }
 
 module.exports = nextConfig 
